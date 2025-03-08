7b9cad4c7ac3b3e02609635643e4dc1e11e45564
Shashwat-yadav-cloud <shashwatyadav576@gmail.com>
2025-03-08 04:25:11 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" ; //treat all js code as newer version
+
+let age = 18
+
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+// symbol = unique
+
+console.log(typeof age)
+
+console.log(typeof undefined ); //undefined
+
+console.log(typeof null); //object
---
+++ basics/xyz.js
@@ -0,0 +1,7 @@
+const accId = "1234"
+let accemail = "shash@gmail.com"
+var city = "bengluru"
+
+accemail  = "shrish@gmail.com"
+city = "mumbai"
+console.table([accId,accemail,city])
