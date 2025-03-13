d238418d62728edae44cc9d0d34942d472374abc
Shashwat-yadav-cloud <shashwatyadav576@gmail.com>
2025-03-13 18:47:42 +0000

    js

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -51,3 +51,51 @@ promiseFour.then((user) => {
 }).catch(function(error){
     console.log(error);
 }).finally(()=> console.log('the promise is either resolved or rejected '))
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:'shrish yadav', password:'123'})
+        } else{
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+
+        const response = await promiseFive
+        console.log(response);
+    } catch (error){
+        console.log(error);
+    }
+
+    
+}
+
+consumePromiseFive()
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
+    }
+}
+
+getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+
+})
+.then((data) => {
+    console.log(data);
+})
+.catch()
